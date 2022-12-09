297d7465e8cecf87f5f4a059f2f1179ddc5dc46d
Abhinav Narayan <abhiryan98@gmail.com>
2022-12-08 18:28:17 -0600

    Update DFT_nofft.py

--- N point DFT/DFT_nofft.py
+++ N point DFT/DFT_nofft.py
@@ -1,42 +1,19 @@
 # -*- coding: utf-8 -*-
-"""
-Calculating DFT without FFT function
-"""
-
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.fftpack import fft
 
-xn = [1,-1,2,3,4]
-N = 8
-pp = fft(xn,n=8)
-for ix in range(len(xn)):
-    if len(xn)<N:
-        xn.append((N-len(xn))*0)
+"""
+N point DFT function
 
+"""
+inp_sig = [1,1,1,1,0,0,0,0]
+
+N = len(inp_sig)
+
+#print(N)
+X = []
+#Compute DFT 
 for k in range(0,N):
-    yn=0
-    o=[]
+    Xk = 0
     for n in range(0,N):
-        yn = yn + np.sum(xn[n] * np.exp(-1j*2*np.pi*k*n/N))
-        o.append(yn)
-
-# for p in range(len(o)):
-#     ab=0
-#     a=[]
-#     for q in range(0,N-1):
-#         ab = ()
-#         a.append(ab)
-
-        
-plt.stem(xn,use_line_collection = True)
-plt.title('input sequence')
-plt.xlabel('n')
-plt.ylabel('Amplitude')
-plt.show()
-
-plt.stem(np.abs(o),use_line_collection = True)
-plt.title('DFT seq')
-plt.xlabel('K')
-plt.ylabel('Amplitude')
-plt.show()
+        Xk += inp_sig[n] * np.exp((-1.0j*2*np.pi*k*n)/N)
+    print(Xk)
